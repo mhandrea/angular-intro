4c27da8585ca79a542e8e3576fabad81c793c65a
Michael Andrea <michaelh.andrea@gmail.com>
2014-11-04 12:11:03 -0800

    finished angular intro

--- js/app.js
+++ js/app.js
@@ -5,3 +5,28 @@
 
 "use strict";
 
+angular.module('AngularDemo', [])
+	.controller('HelloController', function($scope) {
+		$scope.person = {
+			first_name: null,
+			last_name: null
+		};
+	})
+	.controller('MoviesController', function($scope) {
+		$scope.movies = movies;
+		$scope.sortCol = 'rank';
+		$scope.sortBy = function(sortCol) {
+			if ($scope.sortCol == sortCol) {
+				$scope.sortReverse = !$scope.sortReverse;
+			}
+			else {
+				$scope.sortCol = sortCol;
+				$scope.sortReverse = false;
+			}
+			$scope.sortCol = sortCol;
+		};
+		$scope.isSortedBy = function(colName) {
+			return colName == $scope.sortCol;
+		}
+	});
+
